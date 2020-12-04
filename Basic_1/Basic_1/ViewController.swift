4c9a284bbd585d134b14966ffd7bb672194b4be8
sanghun0724 <sanghun0724@naver.com>
2020-12-05 01:06:54 +0900

    safeLayoutGuide in code

--- Basic_1/Basic_1/ViewController.swift
+++ Basic_1/Basic_1/ViewController.swift
@@ -21,6 +21,15 @@ class ViewController: UIViewController {
         button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true //기본값이 false 라 꼭 true 해야함
         button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor,constant:  100).isActive = true
         
+        let label = UILabel.init()
+        label.text = "MY LABEL"
+        
+        self.view.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor,constant: 20).isActive = true
+        label.leftAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.leftAnchor, constant: 20).isActive = true
+        
+        
     }
 
 
