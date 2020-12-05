87cea7312039e6f2812af1958fe28f621e334d1d
sanghun0724 <sanghun0724@naver.com>
2020-12-05 19:03:32 +0900

    CustomKeyboard

---
+++ Custom_Keyboard/Custom_Keyboard/CustomKeyboard.swift
@@ -0,0 +1,13 @@
+//
+//  CustomKeyboard.swift
+//  Custom_Keyboard
+//
+//  Created by sangheon on 2020/12/05.
+//
+
+import UIKit
+
+class CustomKeyboard: UIView {
+
+   
+}
---
+++ Custom_Keyboard/Custom_Keyboard/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Custom_Keyboard
+//
+//  Created by sangheon on 2020/12/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var firstTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        //커스텀 키보드 설정 방법
+     //   firstTextField.inputView = "커스텀 키보드 뷰"
+        
+    }
+
+
+}
+
