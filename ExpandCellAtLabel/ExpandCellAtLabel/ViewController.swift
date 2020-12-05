ace08d80d9185df8d9e39606f6364d4383fa279c
sanghun0724 <sanghun0724@naver.com>
2020-12-06 00:27:21 +0900

    ExpandCell

--- ExpandCellAtLabel/ExpandCellAtLabel/ViewController.swift
+++ ExpandCellAtLabel/ExpandCellAtLabel/ViewController.swift
@@ -44,7 +44,7 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return 7
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -63,11 +63,25 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
         return cell
     }
 
+//    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//
+//    }
+    
+    
+    
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         dataModels[indexPath.row].isExpand = !dataModels[indexPath.row].isExpand //반댓값 접혀있을때 열렸을떄 클릭하면
-        
+        //문제? >> autoLayout으로 설정했을경우에는 RowHeight 같은 오토메틱으로 설정되는 부분들이 꼬일수있다
+        //1. heightForRowAt 으로 높이 지정
+        //2. tableView.reloadData() //상황에따라 이걸로 해결되는 경우도 많음
+        //3. tableView.estimatedSectionHeaderHeight = 0
+        //   tableView.estimatedSectionFooterHeight = 0 //어느정도 해결
+       
+        //4.애니메이션 효과 없애는 방법 < 거의 확실히 해결
+        UIView.setAnimationsEnabled(false)
         tableView.reloadRows(at: [indexPath], with: .none)
+        UIView.setAnimationsEnabled(true)
     }
     
     
