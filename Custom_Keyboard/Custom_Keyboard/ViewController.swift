d58a442d35af9c7f4efb9a45b70a46896f964b03
sanghun0724 <sanghun0724@naver.com>
2020-12-05 20:56:48 +0900

    CustomKeyboardUI

--- Custom_Keyboard/Custom_Keyboard/ViewController.swift
+++ Custom_Keyboard/Custom_Keyboard/ViewController.swift
@@ -16,6 +16,11 @@ class ViewController: UIViewController {
         //커스텀 키보드 설정 방법
      //   firstTextField.inputView = "커스텀 키보드 뷰"
         
+        let loadNib = Bundle.main.loadNibNamed("CustomKeyboard", owner: nil, options: nil) //xib 파일정보를 불러옴
+        
+       let myKeyboardView = loadNib?.first as! CustomKeyboard  //키보드 뷰를 불러옴
+        
+        firstTextField.inputView = myKeyboardView
     }
 
 
