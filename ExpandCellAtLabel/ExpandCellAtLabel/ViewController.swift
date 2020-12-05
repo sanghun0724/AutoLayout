5da3673a7f6be49e653221d549610374b7ee467b
sanghun0724 <sanghun0724@naver.com>
2020-12-06 00:09:04 +0900

    ExpandCell

--- ExpandCellAtLabel/ExpandCellAtLabel/ViewController.swift
+++ ExpandCellAtLabel/ExpandCellAtLabel/ViewController.swift
@@ -15,10 +15,32 @@ class ExpandCell:UITableViewCell {
 
 class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
     
-
+    // 필요한 텍스트가 포함
+    // expand 상태
+    
+    //Model
+    struct ExpandDataModel {
+        var description:String
+        var isExpand:Bool
+        
+    }
+    
+    var dataModels = [ExpandDataModel]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let textArray = ["short Text",
+                         "long long long long long long long long long long long long long long long long long long long long long long long long Text",
+                         "short Text",
+                         "long long long long long long long long long long long long long long long long long long long long long long long long Text",
+                         "short Text",
+                         "long long long long long long long long long long long long long long long long long long long long long long long long Text",]
+        for (_, value) in textArray.enumerated() {
+            dataModels.append(ExpandDataModel.init(description: value, isExpand: false))
+            
+        }
+        
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -28,13 +50,26 @@ class ViewController: UIViewController,UITableViewDelegate,UITableViewDataSource
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell:ExpandCell = tableView.dequeueReusableCell(withIdentifier: "expandCell_ID", for: indexPath) as! ExpandCell
         
-        cell.descriptionLabel.text = "aaaa"
+        cell.descriptionLabel.text = dataModels[indexPath.row].description
+        
+        if dataModels[indexPath.row].isExpand == true {
+            cell.descriptionLabel.numberOfLines = 0
+        } else {
+            cell.descriptionLabel.numberOfLines = 1
+        }
+        
+        cell.selectionStyle = .none
         
         return cell
-        
-        
     }
 
-
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        dataModels[indexPath.row].isExpand = !dataModels[indexPath.row].isExpand //반댓값 접혀있을때 열렸을떄 클릭하면
+        
+        tableView.reloadRows(at: [indexPath], with: .none)
+    }
+    
+    
 }
 
