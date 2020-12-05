4185756ab7a4c794a893cb3b4f23abb5db3c3149
sanghun0724 <sanghun0724@naver.com>
2020-12-05 22:10:26 +0900

    CustomKeyboard Code

--- Custom_Keyboard/Custom_Keyboard/CustomKeyboard.swift
+++ Custom_Keyboard/Custom_Keyboard/CustomKeyboard.swift
@@ -6,8 +6,22 @@
 //
 
 import UIKit
+//delegate - 위임
+
+protocol CustomKeyboardDelegate {
+    func keyboardTapped(str:String)
+}
+
 
 class CustomKeyboard: UIView {
-
-   
+    
+    var delegate:CustomKeyboardDelegate? //끊어지지않고 계속 연결이 남아있을수 있기 때문에(메모리누수) 옵셔널로 지정
+    
+    
+    @IBAction func buttonTapped (_ sender:UIButton) {
+        
+        delegate?.keyboardTapped(str: sender.titleLabel!.text!)
+        
+        
+    }
 }
--- Custom_Keyboard/Custom_Keyboard/ViewController.swift
+++ Custom_Keyboard/Custom_Keyboard/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,CustomKeyboardDelegate {
 
     @IBOutlet weak var firstTextField: UITextField!
     override func viewDidLoad() {
@@ -19,10 +19,33 @@ class ViewController: UIViewController {
         let loadNib = Bundle.main.loadNibNamed("CustomKeyboard", owner: nil, options: nil) //xib 파일정보를 불러옴
         
        let myKeyboardView = loadNib?.first as! CustomKeyboard  //키보드 뷰를 불러옴
-        
+        myKeyboardView.delegate = self
         firstTextField.inputView = myKeyboardView
     }
 
+    func keyboardTapped(str: String) {
+        //키보드 눌렀을 때 호출 됨
+        let oldNumber = Int(firstTextField.text!)
+        var newNumber = Int(str)
+        
+        if str == "00" && oldNumber != nil {
+            newNumber = oldNumber! * 100
+        }
+        if str == "000" && oldNumber != nil {
+            newNumber = oldNumber! * 1000
+        }
 
+        
+        guard let hasNumber = newNumber else {
+            return
+        }
+        
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        
+        if let formatted = numberFormatter.string(from: NSNumber(value: hasNumber)) {
+            firstTextField.text = String(describing: formatted)
+        }
+    }
 }
 
