534a9cb28d6ee08bd746b597aae24c4a97d35b28
sanghun0724 <sanghun0724@naver.com>
2020-12-08 01:08:14 +0900

    Fix

---
+++ Chat_App/Chat_App/YourCell.swift
@@ -0,0 +1,23 @@
+//
+//  YourCell.swift
+//  Chat_App
+//
+//  Created by sangheon on 2020/12/08.
+//
+
+import UIKit
+
+class YourCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
